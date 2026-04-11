f024c64a4255722728f2ac09e4a61f240f2d523d
iLikeToCode <archie@archiesbytes.xyz>
2026-04-11 14:27:37 +0100

    initial go

--- flake.nix
+++ flake.nix
@@ -1,21 +1,11 @@
 {
-  description = "Alpine TUI ISO builder";
+  description = "Alpine TUI OS Imager (Go embedded)";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk, fenix }:
+  outputs = { self, nixpkgs }:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
@@ -23,6 +13,9 @@
   {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = with pkgs; [
+        go_1_26
+        gopls
+        go-tools
         xorriso
         squashfsTools
         syslinux
@@ -46,25 +39,6 @@
         sha256 = "sha256-U/tUZvdhLU/2Fr3g9jfwuM0mfX5SrtxwUiD0h+Qx8VA=";
       };
 
-      imager = 
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          target = "x86_64-unknown-linux-musl";
-          toolchain = with fenix.packages.${system}; combine [
-            minimal.cargo
-            minimal.rustc
-            targets.${target}.latest.rust-std
-          ];
-        in
-
-        (naersk.lib.${system}.override {
-          cargo = toolchain;
-          rustc = toolchain;
-        }).buildPackage {
-          src = ./imager;
-          CARGO_BUILD_TARGET = target;
-        };
-
       alpineRootfs = pkgs.fetchurl {
         url = "https://dl-cdn.alpinelinux.org/alpine/v3.22/releases/x86_64/alpine-minirootfs-3.22.0-x86_64.tar.gz";
         sha256 = "sha256-GIeYhONbBxjwF6UP+FteZWgnnpcjP8QoIiKVhf6y+k0=";
@@ -81,59 +55,70 @@
         cpio
       ];
 
+      imager = pkgs.buildGo126Module {
+        pname = "imager";
+        version = "0.1";
+        src = ./imager;
+        vendorHash = null;
+
+        ldflags = [ "-s" "-w" ];
+
+        doCheck = false;
+      };
+
       buildPhase = ''
+        export HOME=$TMPDIR
         mkdir -p work
         cd work
 
-        # extract minimal Alpine rootfs
+        # Use prebuilt Go binary from buildGoModule
+        cp ${pkgs.lib.getExe imager} ./build/imager
+
+        # Extract Alpine rootfs
         mkdir rootfs
         tar -xzf $alpineRootfs -C rootfs
-        
-        # inject Rust TUI binary
-        mkdir -p rootfs/usr/local/bin
-        cp $imager/bin/imager rootfs/usr/local/bin/
 
-        # simple init script for initramfs
+        # Inject Go binary
+        mkdir -p rootfs/usr/local/bin
+        cp build/imager rootfs/usr/local/bin/
+
+        # Init script
         cat > rootfs/init <<'EOF'
 #!/bin/sh
 mount -t proc proc /proc
 mount -t sysfs sys /sys
 mount -t devtmpfs udev /dev
 /usr/local/bin/imager
-#poweroff -f
 sh
 EOF
         chmod +x rootfs/init
+
         mkdir -p iso/boot
 
-        # create initramfs
+        # Create initramfs
         cd rootfs
         find . | cpio -o -H newc | gzip > ../iso/boot/initramfs.cpio.gz
         cd ..
 
-        # copy kernel from Alpine netboot
+        # Copy kernel
         tar -xzf $alpineNetboot --strip-components=1 -C iso/boot boot/vmlinuz-lts
 
-        # prepare ISO tree for Syslinux
         mkdir -p iso/boot/syslinux
       '';
 
       installPhase = ''
         mkdir -p $out
 
-        # copy syslinux bootloader
         cp -r ${pkgs.syslinux}/share/syslinux/* iso/boot/syslinux/
 
-        # generate minimal isolinux.cfg
         cat > iso/boot/syslinux/isolinux.cfg <<'EOF'
 DEFAULT alpine
 LABEL alpine
   KERNEL /boot/vmlinuz-lts
   INITRD /boot/initramfs.cpio.gz
-  APPEND rw
+  APPEND rw quiet
 EOF
 
-        # build ISO
         xorriso -as mkisofs \
           -o $out/${pname}.iso \
           -b boot/syslinux/isolinux.bin \
@@ -145,15 +130,15 @@ EOF
       '';
     };
 
-    apps.${system}.default = {
+    apps.${system}.test-vm = {
       type = "app";
       program = "${pkgs.writeShellScript "run-iso" ''
         ISO=$(ls ${self.packages.${system}.iso}/*.iso | head -n1)
         exec ${pkgs.qemu}/bin/qemu-system-x86_64 \
           -enable-kvm \
           -cpu host \
-          -m 1024 \
-          -smp $(nproc) \
+          -m 4096 \
+          -smp 2 \
           -cdrom "$ISO"
       ''}";
     };
